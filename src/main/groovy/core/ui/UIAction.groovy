9f230c6910d3e782e9fcb58adf85559319835acb
rizky satria nugraha <rizkysatrian@gmail.com>
2026-01-15 17:00:38 +0700

    update action wait

--- src/main/groovy/core/ui/UIAction.groovy
+++ src/main/groovy/core/ui/UIAction.groovy
@@ -1,14 +1,21 @@
 package core.ui
 
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import core.interaction.TapHandler
 import core.interaction.TextHandler
-import core.types.Timeout
 import core.types.SwipePosition
+import core.types.Timeout
 
 class UIAction {
 
     static void tap(String key, Timeout timeout = Timeout.MEDIUM) {
         TapHandler.handle(key, timeout)
+    }
+
+    static void tapWithRetry(String key, int retry = 2, Timeout timeout = Timeout.SHORT) {
+        retry(retry) {
+            TapHandler.handle(key, timeout)
+        }
     }
 
     static void setText(String key, String text, Timeout timeout = Timeout.MEDIUM) {
@@ -19,22 +26,47 @@ class UIAction {
         return  TextHandler.getText(key, timeout)
     }
 
-    static void swipe(
-        SwipePosition position,
-        int times = 5,
-        double startPct = 0.75,
-        double endPct = 0.25
-    ) {
-        // SwipeUseCase.execute(position, times, startPct, endPct)
+    static void pressBack() {
+        Mobile.pressBack()
     }
 
-    static void swipeToElement(
-        String key,
-        SwipePosition position,
-        int times = 5,
-        double startPct = 0.75,
-        double endPct = 0.25
-    ) {
-        // SwipeUseCase.executeUntilVisible(key, position, times, startPct, endPct)
+    static void waitVeryShort() {
+        wait(Timeout.VERY_SHORT)
+    }
+
+    static void waitShort() {
+        wait(Timeout.SHORT)
+    }
+
+    static void waitMedium() {
+        wait(Timeout.MEDIUM)
+    }
+
+    static void waitLong() {
+        wait(Timeout.LONG)
+    }
+
+    static void waitVeryLong() {
+        wait(Timeout.VERY_LONG)
+    }
+
+    //PRIVATE METHOD
+
+    private static void wait(Timeout timeout) {
+        Mobile.delay(timeout.seconds)
+    }
+
+    private static void retry(int count, Closure action) {
+        Exception last
+        count.times {
+            try {
+                action.call()
+                return
+            } catch (Exception e) {
+                last = e
+                LogUtil.warn("Retry failed:", e.message)
+            }
+        }
+        throw last
     }
 }
